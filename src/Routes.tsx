9ebf582dd66c21dd6adaadd524ac907dfe96f7d4
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-04-30 16:01:57 +0300

    Fix absolute imports

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import Home from '@pages/Home'
-import Product from '@pages/Product'
+import Home from 'pages/Home'
+import Product from 'pages/Product'
 
 const Routes = () => (
   <Switch>
