cc43d07f4c2f797b441034f201daf7e30a771bff
Fernando Balderas Guzmán <fernando.bguzman@gmail.com>
2022-05-11 17:57:40 +0300

    Show items counter in cart's icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { updateThemeKey } from 'redux/actions'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import Badge from 'react-bootstrap/Badge'
 import {
   FaGlobeEurope,
   FaShoppingCart,
@@ -13,12 +14,13 @@ import {
 } from 'react-icons/fa'
 import ThemeContext, { themes } from 'contexts/Theme'
 
-import { ThemeKey, ThemeColors } from 'types'
+import { ThemeKey, ThemeColors, AppState } from 'types'
 import { updateCartOpen } from '../redux/actions'
 
 function NavBar() {
   const dispatch = useDispatch()
   const theme = useContext(ThemeContext)
+  const { inCart } = useSelector((state: AppState) => state.country)
 
   const handleSwitchTheme = (v: ThemeKey) => () => dispatch(updateThemeKey(v))
   const handleShowCart = () => dispatch(updateCartOpen(true))
@@ -55,6 +57,7 @@ function NavBar() {
                 size="1.6em"
                 style={{ color: theme.foreground }}
               />
+              {inCart.length > 0 && <Badge bg="danger">{inCart.length}</Badge>}
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
